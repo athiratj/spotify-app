7e4b3854fb57cc10d5e9ccf8f5ec2586312a7a7e
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-08-07 22:45:13 +0530

    installed spotifywebapi and called its instance in project

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./components/Login";
 import { getTokenFromResponse } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
 
+const s = new SpotifyWebApi();
 function App() {
-  const [token, setToken] = useState();
+  const [token, setToken] = useState(null);
   useEffect(() => {
     // Set token
     const hash = getTokenFromResponse();
