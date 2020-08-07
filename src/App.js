5ca774be3d98460bc2bdd0eff7babc49aa323709
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-08-07 20:46:50 +0530

    added function to pull access token from url

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Login from "./components/Login";
+import { getTokenFromResponse } from "./spotify";
 
 function App() {
+  useEffect(() => {
+    // Set token
+    const hash = getTokenFromResponse();
+    console.log(hash);
+  }, []);
+
   return (
     <div className="app">
       <Login />
