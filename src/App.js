155b22e2946f511f884023ecb4bfe5839d25db8b
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-08-07 20:59:32 +0530

    stored the pulled token from url into a state and setup rendering

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./components/Login";
 import { getTokenFromResponse } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState();
   useEffect(() => {
     // Set token
     const hash = getTokenFromResponse();
-    console.log(hash);
+    window.location.hash = "";
+    let _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+    console.log(token);
   }, []);
 
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  );
+  return <div className="app">{token ? <h1>Logged</h1> : <Login />}</div>;
 }
 
 export default App;
