65b28dd55e764ac6a7489304d5e3e807424f8b58
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-08-08 22:57:21 +0530

    added a single component for sidebar options

---
+++ src/components/SidebarOptions.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SidebarOptions.css";
+
+function SidebarOptions({ title, Icon }) {
+  return <div className="sidebarOptions"></div>;
+}
+
+export default SidebarOptions;
