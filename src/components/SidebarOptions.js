4a5a3fb85c138f9dca73a1a2477e57515def97f6
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-08-08 23:23:04 +0530

    added material ui icons to sidebar

--- src/components/SidebarOptions.js
+++ src/components/SidebarOptions.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./SidebarOptions.css";
 
 function SidebarOptions({ title, Icon }) {
-  return <div className="sidebarOptions">{title}</div>;
+  return (
+    <div className="sidebarOptions">
+      {Icon && <Icon className="sidebarOptions__icon" />}
+      {Icon ? <h4>{title}</h4> : <p>{title}</p>}
+    </div>
+  );
 }
 
 export default SidebarOptions;
