785a9550cb7b43f0c43348105b85f6b5fb71e7f1
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-08-08 22:14:15 +0530

    added different components to player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Sidebar from "./Sidebar";
+import Body from "./Body";
+import Footer from "./Footer";
 
-function Player() {
+function Player({ spotify }) {
   return (
-    <div>
-      <h1>Welcome to Spotify</h1>
+    <div className="player">
+      <div className="player__body">
+        <Sidebar />
+        <Body spotify={spotify} />
+      </div>
+      <Footer spotify={spotify} />
     </div>
   );
 }
