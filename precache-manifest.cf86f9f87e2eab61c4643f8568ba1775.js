74f62ef9c93e8b22cb1d18a7af217d70b571f67c
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-08-10 23:07:03 +0530

    Updates

---
+++ precache-manifest.cf86f9f87e2eab61c4643f8568ba1775.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f353dd4c8b30ab6f7113f8bc6f966e2",
+    "url": "/spotify-app/index.html"
+  },
+  {
+    "revision": "897499218d2154310a36",
+    "url": "/spotify-app/static/css/main.f3e0d5c7.chunk.css"
+  },
+  {
+    "revision": "756ccf4362d57cdbc120",
+    "url": "/spotify-app/static/js/2.31a5e07f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spotify-app/static/js/2.31a5e07f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "897499218d2154310a36",
+    "url": "/spotify-app/static/js/main.12bdf5f5.chunk.js"
+  },
+  {
+    "revision": "d5b09f2c82a10039653b",
+    "url": "/spotify-app/static/js/runtime-main.9d9fe14a.js"
+  }
+]);
