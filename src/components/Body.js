994342d4d20ac141d600cb998662d91a1d65fce2
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-08-10 13:37:27 +0530

    added more styling to body and icons

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./Body.css";
 import Header from "./Header";
 import { useDataLayerValue } from "./Datalayer";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 function Body({ spotify }) {
   const [{ discover_weekly }, dispatch] = useDataLayerValue();
@@ -14,6 +17,13 @@ function Body({ spotify }) {
           <strong>PLAYLIST</strong>
           <h2>Discover Weekly</h2>
           <p>{discover_weekly?.description}</p>
+        </div>
+      </div>
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilledIcon className="body__shuffle" />
+          <FavoriteIcon fontSize="large" />
+          <MoreHorizIcon />
         </div>
       </div>
     </div>
