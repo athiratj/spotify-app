9a52f6cac06e6a49b1bf1ccc53698cdb60edcc20
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-08-10 22:41:52 +0530

    added more functions for setting token

--- src/App.js
+++ src/App.js
@@ -15,32 +15,49 @@ function App() {
     const hash = getTokenFromResponse();
     window.location.hash = "";
     let _token = hash.access_token;
+
     if (_token) {
+      s.setAccessToken(_token);
+
       dispatch({
         type: "SET_TOKEN",
         token: _token,
       });
-      s.setAccessToken(_token);
-      s.getMe().then((user) => {
-        dispatch({
-          type: "SET_USER",
-          user: user,
-        });
-      });
-      s.getUserPlaylists().then((playlists) => {
-        dispatch({
-          type: "SET_PLAYLISTS",
-          playlists,
-        });
-      });
+
       s.getPlaylist("17I8Yohdh2wEDRaeBg7N6V").then((response) =>
         dispatch({
           type: "SET_DISCOVER_WEEKLY",
           discover_weekly: response,
         })
       );
+
+      s.getMyTopArtists().then((response) =>
+        dispatch({
+          type: "SET_TOP_ARTISTS",
+          top_artists: response,
+        })
+      );
+
+      dispatch({
+        type: "SET_SPOTIFY",
+        spotify: s,
+      });
+
+      s.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user,
+        });
+      });
+
+      s.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists,
+        });
+      });
     }
-  }, []);
+  }, [token, dispatch]);
 
   return (
     <div className="app">{token ? <Player spotify={s} /> : <Login />}</div>
--- src/components/Body.js
+++ src/components/Body.js
@@ -9,17 +9,20 @@ import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 function Body({ spotify }) {
   const [{ discover_weekly }, dispatch] = useDataLayerValue();
+
   return (
     <div className="body">
       <Header spotify={spotify} />
+
       <div className="body__info">
         <img src={discover_weekly?.images[0].url} alt="" />
         <div className="body__infoText">
           <strong>PLAYLIST</strong>
-          <h2>Discover Weekly</h2>
+          <h2>{discover_weekly?.name}</h2>
           <p>{discover_weekly?.description}</p>
         </div>
       </div>
+
       <div className="body__songs">
         <div className="body__icons">
           <PlayCircleFilledIcon className="body__shuffle" />
